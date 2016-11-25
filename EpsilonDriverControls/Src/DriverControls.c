e94f0482a971d79f22974d99f0cef5160320171c
Enoch Tsang <enoch.tsang@calgaryscientific.com>
2016-11-24 17:35:43 -0700

    use |= to be more explicit

--- EpsilonDriverControls/Src/DriverControls.c
+++ EpsilonDriverControls/Src/DriverControls.c
@@ -43,13 +43,13 @@ void sendLightsTask(void const* arg)
         hcan2.pTxMsg->StdId = LIGHTS_STDID;
         hcan2.pTxMsg->DLC = LIGHTS_DLC;
         memset(hcan2.pTxMsg->Data, 0, 1);
-        hcan2.pTxMsg->Data[0] += 0x01 * !HAL_GPIO_ReadPin(HEADLIGHTS_OFF_GPIO_Port, HEADLIGHTS_OFF_Pin);
-        hcan2.pTxMsg->Data[0] += 0x02 * !HAL_GPIO_ReadPin(HEADLIGHTS_LOW_GPIO_Port, HEADLIGHTS_LOW_Pin);
-        hcan2.pTxMsg->Data[0] += 0x04 * !HAL_GPIO_ReadPin(HEADLIGHTS_HIGH_GPIO_Port, HEADLIGHTS_HIGH_Pin);
-        hcan2.pTxMsg->Data[0] += 0x08 * !HAL_GPIO_ReadPin(RSIGNAL_GPIO_Port, RSIGNAL_Pin);
-        hcan2.pTxMsg->Data[0] += 0x10 * !HAL_GPIO_ReadPin(LSIGNAL_GPIO_Port, LSIGNAL_Pin);
-        hcan2.pTxMsg->Data[0] += 0x20 * !HAL_GPIO_ReadPin(HAZARDS_GPIO_Port, HAZARDS_Pin);
-        hcan2.pTxMsg->Data[0] += 0x40 * !HAL_GPIO_ReadPin(INTERIOR_GPIO_Port, INTERIOR_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x01 * !HAL_GPIO_ReadPin(HEADLIGHTS_OFF_GPIO_Port, HEADLIGHTS_OFF_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x02 * !HAL_GPIO_ReadPin(HEADLIGHTS_LOW_GPIO_Port, HEADLIGHTS_LOW_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x04 * !HAL_GPIO_ReadPin(HEADLIGHTS_HIGH_GPIO_Port, HEADLIGHTS_HIGH_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x08 * !HAL_GPIO_ReadPin(RSIGNAL_GPIO_Port, RSIGNAL_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x10 * !HAL_GPIO_ReadPin(LSIGNAL_GPIO_Port, LSIGNAL_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x20 * !HAL_GPIO_ReadPin(HAZARDS_GPIO_Port, HAZARDS_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x40 * !HAL_GPIO_ReadPin(INTERIOR_GPIO_Port, INTERIOR_Pin);
         HAL_CAN_Transmit_IT(&hcan2);
         osMutexRelease(canHandleMutex);
     }
@@ -72,11 +72,11 @@ void sendMusicTask(void const* arg)
         hcan2.pTxMsg->StdId = MUSIC_STDID;
         hcan2.pTxMsg->DLC = MUSIC_DLC;
         memset(hcan2.pTxMsg->Data, 0, 1);
-        hcan2.pTxMsg->Data[0] += 0x01 * !HAL_GPIO_ReadPin(AUX_GPIO_Port, AUX_Pin);
-        hcan2.pTxMsg->Data[0] += 0x02 * !HAL_GPIO_ReadPin(VOLUME_UP_GPIO_Port, VOLUME_UP_Pin);
-        hcan2.pTxMsg->Data[0] += 0x03 * !HAL_GPIO_ReadPin(VOLUME_DOWN_GPIO_Port, VOLUME_DOWN_Pin);
-        hcan2.pTxMsg->Data[0] += 0x04 * !HAL_GPIO_ReadPin(NEXT_SONG_GPIO_Port, NEXT_SONG_Pin);
-        hcan2.pTxMsg->Data[0] += 0x10 * !HAL_GPIO_ReadPin(LAST_SONG_GPIO_Port, LAST_SONG_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x01 * !HAL_GPIO_ReadPin(AUX_GPIO_Port, AUX_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x02 * !HAL_GPIO_ReadPin(VOLUME_UP_GPIO_Port, VOLUME_UP_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x03 * !HAL_GPIO_ReadPin(VOLUME_DOWN_GPIO_Port, VOLUME_DOWN_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x04 * !HAL_GPIO_ReadPin(NEXT_SONG_GPIO_Port, NEXT_SONG_Pin);
+        hcan2.pTxMsg->Data[0] |= 0x10 * !HAL_GPIO_ReadPin(LAST_SONG_GPIO_Port, LAST_SONG_Pin);
         HAL_CAN_Transmit_IT(&hcan2);
         osMutexRelease(canHandleMutex);
     }
@@ -112,8 +112,8 @@ void sendDriverTask(void const* arg)
             regen = 0;
         }
 
-        hcan2.pTxMsg->Data[0] += (regen & 0x000000ffUL);
-        hcan2.pTxMsg->Data[1] += (regen & 0x00000f00UL) >> 8; // Use first 4 bits
+        hcan2.pTxMsg->Data[0] |= (regen & 0x000000ffUL);
+        hcan2.pTxMsg->Data[1] |= (regen & 0x00000f00UL) >> 8; // Use first 4 bits|
 
         // Acceleration 12bit analog input
         if (HAL_ADC_PollForConversion(&hadc2, ADC_POLL_TIMEOUT) == HAL_OK)
@@ -125,15 +125,15 @@ void sendDriverTask(void const* arg)
             accel = 0;
         }
 
-        hcan2.pTxMsg->Data[1] += (accel & 0x0000000fUL) << 4; // Use last 4 bits
-        hcan2.pTxMsg->Data[2] += (accel & 0x00000ff0UL) >> 4;
+        hcan2.pTxMsg->Data[1] |= (accel & 0x0000000fUL) << 4; // Use last 4 bits
+        hcan2.pTxMsg->Data[2] |= (accel & 0x00000ff0UL) >> 4;
         // GPIO Inputs
-        hcan2.pTxMsg->Data[3] += 0x01 * !HAL_GPIO_ReadPin(BRAKES_GPIO_Port, BRAKES_Pin);
-        hcan2.pTxMsg->Data[3] += 0x02 * !HAL_GPIO_ReadPin(FORWARD_GPIO_Port, FORWARD_Pin);
-        hcan2.pTxMsg->Data[3] += 0x04 * !HAL_GPIO_ReadPin(REVERSE_GPIO_Port, REVERSE_Pin);
-        hcan2.pTxMsg->Data[3] += 0x08 * !HAL_GPIO_ReadPin(PUSH_TO_TALK_GPIO_Port, PUSH_TO_TALK_Pin);
-        hcan2.pTxMsg->Data[3] += 0x10 * !HAL_GPIO_ReadPin(HORN_GPIO_Port, HORN_Pin);
-        hcan2.pTxMsg->Data[3] += 0x20 * !HAL_GPIO_ReadPin(RESET_GPIO_Port, RESET_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x01 * !HAL_GPIO_ReadPin(BRAKES_GPIO_Port, BRAKES_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x02 * !HAL_GPIO_ReadPin(FORWARD_GPIO_Port, FORWARD_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x04 * !HAL_GPIO_ReadPin(REVERSE_GPIO_Port, REVERSE_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x08 * !HAL_GPIO_ReadPin(PUSH_TO_TALK_GPIO_Port, PUSH_TO_TALK_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x10 * !HAL_GPIO_ReadPin(HORN_GPIO_Port, HORN_Pin);
+        hcan2.pTxMsg->Data[3] |= 0x20 * !HAL_GPIO_ReadPin(RESET_GPIO_Port, RESET_Pin);
         HAL_CAN_Transmit_IT(&hcan2);
         osMutexRelease(canHandleMutex);
     }
