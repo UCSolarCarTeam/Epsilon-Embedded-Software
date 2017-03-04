d1215f0bd99282e73bb2dda55e816b62f624589c
Enoch Tsang <etsang1@hotmail.com>
2017-03-04 16:01:16 -0700

    use rising and falling edge for horn in

--- EpsilonCCS/Src/main.c
+++ EpsilonCCS/Src/main.c
@@ -321,7 +321,7 @@ static void MX_GPIO_Init(void)
 
     /*Configure GPIO pin : HORN_IN_Pin */
     GPIO_InitStruct.Pin = HORN_IN_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(HORN_IN_GPIO_Port, &GPIO_InitStruct);
 
