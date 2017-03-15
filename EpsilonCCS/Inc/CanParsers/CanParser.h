af6fa2739872c392fbe936d44e69c4e3d2935a9b
Enoch Tsang <etsang1@hotmail.com>
2017-03-14 18:22:06 -0600

    stubs for CanParser

---
+++ EpsilonCCS/Inc/CanParsers/CanParser.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_can.h"
+
+void parseCanMessage(uint32_t stdId, uint8_t data[8]);
---
+++ EpsilonCCS/Src/CanParsers/CanParser.c
@@ -0,0 +1,32 @@
+#include "CanParser.h"
+
+#include "AuxBmsCanParser.h"
+#include "BmsCanParser.h"
+#include "DriverControlsCanParser.h"
+#include "LightsCanParser.h"
+#include "MotorCanParser.h"
+#include "MpptCanParser.h"
+
+// 0x720 >= AUX_BMS >= 0x723
+#define AUX_BMS_CAN_ID 0x720
+#define AUX_BMS_CAN_MASK 0xFFC
+// 0x300 >= BMS >= 0x307
+#define BMS_CAN_ID 0x300
+#define BMS_CAN_MASK 0xFF8
+// 0x700 >= DRIVER_CONTROL >= 0x703
+#define DRIVER_CONTROL_CAN_ID 0x700
+#define DRIVER_CONTROL_CAN_MASK 0xFFC
+// 0x710 >= LIGHTS 0x713
+#define LIGHTS_CAN_ID 0x710
+#define LIGHTS_CAN_MASK 0xFFC
+// 0x400 >= MOTOR >= 0x43F
+#define MOTOR_CAN_ID 0x400
+#define MOTOR_CAN_MASK 0xFC0
+// 0x600 >= MPPT >= 0x600
+#define MPPT_CAN_ID 0x600
+#define MPPT_CAN_MASK 0xFFF
+
+void parseCanMessage(uint32_t stdId, uint8_t data[8])
+{
+
+}
