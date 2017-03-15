5265e2dfbb18ac6f5efff8e86da48f58ff264457
Enoch Tsang <etsang1@hotmail.com>
2017-03-14 18:43:36 -0600

    stubs for AuxBmsCanParser

---
+++ EpsilonCCS/Src/CanParsers/AuxBmsCanParser.c
@@ -0,0 +1,31 @@
+#include "AuxBmsCanParser.h"
+
+void parseBmuCanMessage(uint32_t stdId, uint8_t data[8])
+{
+
+}
+
+void parseBmuHeartbeat(uint8_t data[8])
+{
+
+}
+
+void parseStartupInfo(uint8_t data[8])
+{
+
+}
+
+void parsePackInfo(uint8_t data[8])
+{
+
+}
+
+void parseErrorsTempInfo(uint8_t data[8])
+{
+
+}
+
+void parseCellVoltages(uint8_t data[8])
+{
+
+}
