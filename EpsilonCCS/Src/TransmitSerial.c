fc421c2c92c15b0956d70db3d811272257641a82
Enoch Tsang <enoch.tsang@calgaryscientific.com>
2017-03-28 16:42:51 -0600

    remove unused variable

--- EpsilonCCS/Src/TransmitSerial.c
+++ EpsilonCCS/Src/TransmitSerial.c
@@ -2,8 +2,6 @@
 
 #include "stm32f4xx_hal_uart.h"
 
-static unsigned char UartReady = 1;
-
 void transmitMessage(uint8_t* payload, uint16_t bufferSize)
 {
     HAL_UART_Transmit(&huart3, payload, bufferSize, 5000);
