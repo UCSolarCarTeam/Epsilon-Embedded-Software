429e2f99ce2252c6c5e1747906486ea8fd5d1c01
Enoch Tsang <enoch.tsang@calgaryscientific.com>
2017-03-28 15:44:32 -0600

    Use blocking operation

--- EpsilonCCS/Src/TransmitSerial.c
+++ EpsilonCCS/Src/TransmitSerial.c
@@ -6,18 +6,5 @@ static unsigned char UartReady = 1;
 
 void transmitMessage(uint8_t* payload, uint16_t bufferSize)
 {
-    while (!UartReady)
-    {
-    }
-
-    UartReady = 0; // False
-    HAL_UART_Transmit_DMA(&huart3, payload, bufferSize);
-}
-
-// Reimplement from weak implementation at stm32f4xx_hal_uart.c
-// Should get called sometime after transmitMessage() once the transmit is complete
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef* UartHandle)
-{
-    /* Set transmission flag: transfer complete */
-    UartReady = 1; // True
+	HAL_UART_Transmit(&huart3, payload, bufferSize, 5000);
 }
