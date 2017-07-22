2aa792bb4a4085c0a1fbbd2a4a6b99354acea058
bill-luu <billluu619@gmail.com>
2017-07-22 14:15:20 -0600

    pack battery faults

--- EpsilonCCS/Src/TelemetryReporting/TelemetryReporting.c
+++ EpsilonCCS/Src/TelemetryReporting/TelemetryReporting.c
@@ -8,11 +8,14 @@
 #include "MotorDetailsData.h"
 #include "DriverControlData.h"
 #include "MotorFaultsData.h"
+#include "BatteryFaultsData.h"
 
 #define KEY_MOTOR_LENGTH (43)
 #define MOTOR_DETAILS_LENGTH (69)
 #define DRIVER_CONTROLS_LENGTH (9)
 #define MOTOR_FAULTS_LENGTH (9)
+#define BATTERY_FAULTS_LENGTH (3)
+
 
 #define CCS_TELEM_PERIOD_MS (200) // 5Hz == 200ms
 
@@ -186,7 +189,15 @@ void sendMotorFaults()
 
 void sendBatteryFaults()
 {
+    unsigned int unframedPacketLength = MOTOR_FAULTS_LENGTH + CHECKSUM_LENGTH;
+    unsigned char packetPayload[unframedPacketLength];
 
+    packetPayload[0] = BATTERY_FAULTS_PKG_ID;
+    writeBoolsIntoArray(packetPayload, 1, &batteryFaultsData.batteryErrorFlags, 21);
+    writeBoolsIntoArray(packetPayload, 2, &batteryFaultsData.batteryLimitFlags, 14);
+    addChecksum(packetPayload, BATTERY_FAULTS_LENGTH);
+    unsigned char packet[unframedPacketLength + FRAMING_LENGTH_INCREASE];
+    unsigned int packetLength = frameData(packetPayload, unframedPacketLength, packet);
 }
 
 void sendBattery()
