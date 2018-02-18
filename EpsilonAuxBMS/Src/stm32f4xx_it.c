bdc00e21d8545dfbbd6632e47e43529b38df76de
Baba Adeniran <babaadeniran@Babas-MBP.cg.shawcable.net>
2018-02-17 23:05:56 -0700

    Added timer interrupt

--- EpsilonAuxBMS/Src/stm32f4xx_it.c
+++ EpsilonAuxBMS/Src/stm32f4xx_it.c
@@ -122,6 +122,20 @@ void EXTI9_5_IRQHandler(void)
   /* USER CODE END EXTI9_5_IRQn 1 */
 }
 
+/**
+* @brief This function handles TIM2 global interrupt.
+*/
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  /* USER CODE END TIM2_IRQn 1 */
+}
+
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
