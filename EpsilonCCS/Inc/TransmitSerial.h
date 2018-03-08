47056d04d7ea211156000ed9651e67b54a1d6053
bill-luu <billluu619@gmail.com>
2018-03-08 11:38:17 -0700

    Updated error handling on USART

--- EpsilonCCS/Inc/TransmitSerial.h
+++ EpsilonCCS/Inc/TransmitSerial.h
@@ -3,6 +3,11 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_uart.h"
 
+#define LED_ON 1
+#define LED_OFF 0
+
+#define USART_TIMEOUT_PERIOD 5000
+
 extern UART_HandleTypeDef huart3;
 
 void transmitMessage(uint8_t* payload, uint16_t bufferSize);
--- EpsilonCCS/Src/TransmitSerial.c
+++ EpsilonCCS/Src/TransmitSerial.c
@@ -1,22 +1,38 @@
 #include "TransmitSerial.h"
 
 #include "stm32f4xx_hal_uart.h"
-#define LIGHT_ON 1
-#define LIGHT_OFF 0
 
+/*
+* Flashing Red LED: The USART line is has errored sending this message, will re-try
+* Solid Red LED: The USART Port has timed out, check if Hermes is on
+*/
 void transmitMessage(uint8_t* payload, uint16_t bufferSize)
 {
-    while (1) //In case of busy
-    {
-        HAL_StatusTypeDef transmitState = HAL_UART_Transmit(&huart3, payload, bufferSize, 5000);
+    uint16_t attemptCount = 0;
 
-        if (transmitState == HAL_TIMEOUT || transmitState == HAL_ERROR)
+    while (1)
+    {
+        HAL_StatusTypeDef transmitState = HAL_UART_Transmit(&huart3, payload, bufferSize, USART_TIMEOUT_PERIOD);
+
+        if(transmitState == HAL_OK)
         {
-            HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LIGHT_ON);
+            HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LED_OFF);
+            break;  
+        }
+        else if(transmitState == HAL_TIMEOUT)
+        {
+            HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LED_ON);
+            attemptCount++;
         }
         else
         {
-            HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LIGHT_OFF);
+            HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+            attemptCount++;
+        }
+
+        if(attemptCount > 4) // Could not send
+        {
+            break;
         }
     }
 }
