6e2de82491c8d6cc929ed0dec961fd9823921f1b
bill-luu <billluu619@gmail.com>
2018-03-30 21:00:50 -0600

    Respond to PR comments

--- EpsilonCCS/Inc/TransmitSerial.h
+++ EpsilonCCS/Inc/TransmitSerial.h
@@ -5,7 +5,7 @@
 
 #define LED_ON 1
 #define LED_OFF 0
-#define NUMBER_OF_ATTEMPTS 2
+#define NUMBER_OF_ATTEMPTS 3
 
 #define USART_TIMEOUT_PERIOD 5000
 
--- EpsilonCCS/Src/TransmitSerial.c
+++ EpsilonCCS/Src/TransmitSerial.c
@@ -4,20 +4,20 @@
 
 /*
 * Flashing Red LED: The USART line is has errored sending this message, will re-try
-* Solid Red LED: The USART Port has timed out, check if Hermes is on
+* Solid Red LED: The USART Port has timed out, check if the other end of the serial port is initialized and listening
 */
 void transmitMessage(uint8_t* payload, uint16_t bufferSize)
 {
     uint16_t attemptCount = 0;
 
-    while (1)
+    while (attemptCount < NUMBER_OF_ATTEMPTS)
     {
         HAL_StatusTypeDef transmitState = HAL_UART_Transmit(&huart3, payload, bufferSize, USART_TIMEOUT_PERIOD);
 
         if (transmitState == HAL_OK)
         {
             HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LED_OFF);
-            break;
+            return;
         }
         else if (transmitState == HAL_TIMEOUT)
         {
@@ -30,9 +30,5 @@ void transmitMessage(uint8_t* payload, uint16_t bufferSize)
             attemptCount++;
         }
 
-        if (attemptCount > NUMBER_OF_ATTEMPTS) // Could not send
-        {
-            break;
-        }
     }
 }
