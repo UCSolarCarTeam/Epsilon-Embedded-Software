501c29032611126729c0baa9b1bec1da12ab441e
Safa <safa.syed@ucalgary.ca>
2018-12-16 22:11:43 -0700

    added checking for number of arguments as well as checking to see if project entered exists

--- gdbflash.sh
+++ gdbflash.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cd $1/build
+if [ $# -ne 2 ]; then
+    echo "Please enter two arguments (Project name and Port number)"
+    exit 1
+fi
+
+if [ -d $1 ]; then
+   	cd $1/build
+else
+   echo "$1 is not a valid project"
+   exit 1
+fi
 
 arm-none-eabi-gdb $1.elf -ex "target extended-remote /dev/ttyACM$2" -ex 'monitor tpwr enable' -ex 'monitor swdp_scan' -ex 'attach 1' -ex 'load'
