98d7cf2956c4018d70cd951513e417a14a589c3a
Bill Luu <billluu619@gmail.com>
2019-07-14 16:00:43 -0600

    Change Aux BMS charge allowance to comply with scrutineering
    According to the VDR - there are 3 events in which the BPS prevents:
    
    Over Current, Under Voltage & Over Voltage should turn off all the contactors.
    
    Over Temperature: Our max charging temp is 45, and the max discharge is 60. If we are out of out charging range but before discharge, it should just disengage the charge contactor.
    
    If the charge contactor is opened due to a temperature trip BUT there is still a negative current applied (IE Car is still somehow charging), the car should trip.
    
    Over/Under Voltage: The car should open the contactors when we hit our upper/lower internal limits(> 4.15 & < 2.60), but trip when the Orion wants it to turn off - above 4.2 and below 2.55.
    
    Over Current: The car should trip when we hit a current below -47 A (charge current) or above 229 A (dischage current).
    
    Also, we dropped functionality for differentiating driverInputs.

--- EpsilonAuxBMS/Inc/UpdateChargeAllowanceTask.h
+++ EpsilonAuxBMS/Inc/UpdateChargeAllowanceTask.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "AuxBmsTasks.h"
 #include "OrionStatus.h"
-#include "DriversInput.h"
 
 // Task for determining the allowance of charge/discharge based on orion cell voltages
 // It will turn off charge contactor if max cell voltage is too high,
 // and discharge contactor is min cell voltage is too low
 // arg: osMutexId* auxStatusMutex
 void updateChargeAllowanceTask(void const* arg);
+
+int chargeShouldTrip();
+
+int dischargeShouldTrip();
--- EpsilonAuxBMS/Src/UpdateChargeAllowanceTask.c
+++ EpsilonAuxBMS/Src/UpdateChargeAllowanceTask.c
@@ -3,12 +3,104 @@
 static const uint32_t CHARGE_ALLOWANCE_UPDATE_FREQ = 100; // Every 100ms
 // These values are subject to change based on what regulations say
 // Ask Dan if any issues
-static const float MAX_CELL_VOLTAGE = 4.05;
-static const float MIN_CELL_VOLTAGE = 2.85;
+static const float ORION_MAX_CELL_VOLTAGE = 4.20; // Much match orion bms value
+static const float ORION_MIN_CELL_VOLTAGE = 2.55;
+
+static const float AUX_BMS_MAX_CELL_VOLTAGE = 4.15;
+static const float AUX_BMS_MIN_CELL_VOLTAGE = 2.60;
+
 static const float DEFAULT_VOLTAGE_UNITS = 0.0001; // From: https://www.orionbms.com/manuals/utility/
 
+static const int ORION_MAX_CHARGE_TEMP = 44;
+static const short ORION_MAX_CHARGE_CURRENT = -47;
+
+static const int ORION_MAX_DISCHARGE_TEMP = 59;
+static const short ORION_MAX_DISCHARGE_CURRENT = 229;
 // From SetContactorsTask.c
 extern void disconnectContactors(uint8_t updateAuxStatus);
+
+// When orion tells aux bms to open the charge contactor,
+// read the state of the car to see if we should trip
+int chargeShouldTrip()
+{
+    int chargeShouldTrip = 0;
+
+    if (orionStatus.canMsgReceived)
+    {
+        int tripDuetoHighCell = 0;
+
+        if (DEFAULT_VOLTAGE_UNITS * orionStatus.maxCellVoltage >= ORION_MAX_CELL_VOLTAGE)
+        {
+            tripDuetoHighCell = 1;
+        }
+
+        // It's ok to just have a high temperature,
+        // as long as the car is not charging
+        int tripDuetoHighTempAndCurrent = 0;
+
+        if (orionStatus.highTemperature >= ORION_MAX_CHARGE_TEMP
+                && orionStatus.packCurrent < 0)
+        {
+            tripDuetoHighTempAndCurrent = 1;
+        }
+
+        int tripDuetoPackCurrent = 0;
+
+        // Charge current is negative
+        if (orionStatus.packCurrent <= ORION_MAX_CHARGE_CURRENT)
+        {
+            tripDuetoPackCurrent = 1;
+        }
+
+        chargeShouldTrip = tripDuetoHighCell ||
+                           tripDuetoHighTempAndCurrent ||
+                           tripDuetoPackCurrent;
+    }
+
+    return chargeShouldTrip;
+}
+
+
+// When orion tells aux bms to open the discharge contactor,
+// read the state of the car to see if we should trip
+
+int dischargeShouldTrip()
+{
+    int dischargeShouldTrip = 0;
+
+    if (orionStatus.canMsgReceived)
+    {
+        int tripDuetoLowCell = 0;
+
+        if (DEFAULT_VOLTAGE_UNITS * orionStatus.minCellVoltage <= ORION_MIN_CELL_VOLTAGE)
+        {
+            tripDuetoLowCell = 1;
+        }
+
+        // It's ok to just have a high temperature,
+        // as long as the car is not discharging
+        int tripDuetoHighTempAndCurrent = 0;
+
+        if (orionStatus.highTemperature >= ORION_MAX_DISCHARGE_TEMP
+                && orionStatus.packCurrent > 0)
+        {
+            tripDuetoHighTempAndCurrent = 1;
+        }
+
+        int tripDuetoPackCurrent = 0;
+
+        if (orionStatus.packCurrent >= ORION_MAX_DISCHARGE_CURRENT)
+        {
+            tripDuetoPackCurrent = 1;
+        }
+
+        dischargeShouldTrip = tripDuetoLowCell ||
+                              tripDuetoHighTempAndCurrent ||
+                              tripDuetoPackCurrent;
+    }
+
+    return dischargeShouldTrip;
+}
 
 void updateChargeAllowanceTask(void const* arg)
 {
@@ -53,79 +145,86 @@ void updateChargeAllowanceTask(void const* arg)
 
         if (auxStatus.startUpSequenceDone)
         {
-            if (driversInput.forward || driversInput.reverse) // In forward or reverse
+            unsigned dischargeSense = HAL_GPIO_ReadPin(ORION_DISCHARGE_ENABLE_SENSE_GPIO_Port, ORION_DISCHARGE_ENABLE_SENSE_Pin);
+            unsigned chargeSense = HAL_GPIO_ReadPin(ORION_CHARGE_ENABLE_SENSE_GPIO_Port, ORION_CHARGE_ENABLE_SENSE_Pin);
+
+            // Check if charge should trip the car
+            int chargeTrippedCar = chargeShouldTrip();
+
+            // Check if dicharge should trip the car
+            int dischargeTrippedCar = dischargeShouldTrip();
+
+            int protectionTripping = 0;
+
+            // If the charge contactor is off, but we are still charging, trip
+            if (!chargeSense && orionStatus.packCurrent < 0)
             {
-                if (!HAL_GPIO_ReadPin(ORION_CHARGE_ENABLE_SENSE_GPIO_Port, ORION_CHARGE_ENABLE_SENSE_Pin) ||
-                        !HAL_GPIO_ReadPin(ORION_DISCHARGE_ENABLE_SENSE_GPIO_Port, ORION_DISCHARGE_ENABLE_SENSE_Pin))
+                protectionTripping = 1;
+            }
+
+            // If both contactors should be off
+            if (chargeTrippedCar
+                    || dischargeTrippedCar
+                    || protectionTripping
+                    || (!dischargeSense && !chargeSense))
+            {
+                // Trip car
+                chargeContactorOverride = 1;
+                dischargeContactorOverride = 1;
+                allowCharge = 0;
+                allowDischarge = 0;
+                shutOff = 1;
+                // Turn all contactors and high voltage enable off
+                disconnectContactors(0);
+            }
+            else
+            {
+                // Turn off discharge without tripping
+                if (!dischargeSense)
+                {
+                    dischargeContactorOverride = 1;
+                    allowDischarge = 0;
+                    HAL_GPIO_WritePin(HV_ENABLE_GPIO_Port, HV_ENABLE_Pin, GPIO_PIN_RESET);
+                    HAL_GPIO_WritePin(DISCHARGE_ENABLE_GPIO_Port, DISCHARGE_ENABLE_Pin, GPIO_PIN_RESET);
+                }
+
+                // Turn off charge without tripping
+                if (!chargeSense)
                 {
                     chargeContactorOverride = 1;
-                    dischargeContactorOverride = 1;
                     allowCharge = 0;
-                    allowDischarge = 0;
-                    shutOff = 1;
-                    // Turn all contactors and high voltage enable off
-                    disconnectContactors(0);
+                    HAL_GPIO_WritePin(CHARGE_ENABLE_GPIO_Port, CHARGE_ENABLE_Pin, GPIO_PIN_RESET);
                 }
             }
-            else // Aux mode
+        }
+
+        if (orionStatus.canMsgReceived)
+        {
+            if (DEFAULT_VOLTAGE_UNITS * orionStatus.maxCellVoltage > AUX_BMS_MAX_CELL_VOLTAGE)
             {
-                if (!HAL_GPIO_ReadPin(ORION_CHARGE_ENABLE_SENSE_GPIO_Port, ORION_CHARGE_ENABLE_SENSE_Pin))
+                voltagesInRange = 0;
+
+                if (allowCharge) // To avoid wasting time writing to the pin again
                 {
-                    chargeContactorOverride = 1;
                     allowCharge = 0;
+                    chargeContactorOverride = 1;
                     // Turn off charge contactor
                     HAL_GPIO_WritePin(CHARGE_ENABLE_GPIO_Port, CHARGE_ENABLE_Pin, GPIO_PIN_RESET);
-
-                    if (!HAL_GPIO_ReadPin(ORION_DISCHARGE_ENABLE_SENSE_GPIO_Port, ORION_DISCHARGE_ENABLE_SENSE_Pin))
-                    {
-                        dischargeContactorOverride = 1;
-                        allowDischarge = 0;
-                        shutOff = 1;
-                        // Turn off common and discharge contactor, and high voltage enable
-                        HAL_GPIO_WritePin(HV_ENABLE_GPIO_Port, HV_ENABLE_Pin, GPIO_PIN_RESET);
-                        HAL_GPIO_WritePin(COMMON_ENABLE_GPIO_Port, COMMON_ENABLE_Pin, GPIO_PIN_RESET);
-                        HAL_GPIO_WritePin(DISCHARGE_ENABLE_GPIO_Port, DISCHARGE_ENABLE_Pin, GPIO_PIN_RESET);
-                    }
                 }
-                else
+            }
+
+            if (DEFAULT_VOLTAGE_UNITS * orionStatus.minCellVoltage < AUX_BMS_MIN_CELL_VOLTAGE)
+            {
+                voltagesInRange = 0;
+
+                if (allowDischarge) // To avoid wasting time writing to the pin again
                 {
-                    if (!HAL_GPIO_ReadPin(ORION_DISCHARGE_ENABLE_SENSE_GPIO_Port, ORION_DISCHARGE_ENABLE_SENSE_Pin))
-                    {
-                        dischargeContactorOverride = 1;
-                        allowDischarge = 0;
-                        // Turn off discharge contactor and high voltage enable
-                        HAL_GPIO_WritePin(HV_ENABLE_GPIO_Port, HV_ENABLE_Pin, GPIO_PIN_RESET);
-                        HAL_GPIO_WritePin(DISCHARGE_ENABLE_GPIO_Port, DISCHARGE_ENABLE_Pin, GPIO_PIN_RESET);
-                    }
+                    allowDischarge = 0;
+                    dischargeContactorOverride = 1;
+                    // Turn off High voltage enable and discharge contactor
+                    HAL_GPIO_WritePin(HV_ENABLE_GPIO_Port, HV_ENABLE_Pin, GPIO_PIN_RESET);
+                    HAL_GPIO_WritePin(DISCHARGE_ENABLE_GPIO_Port, DISCHARGE_ENABLE_Pin, GPIO_PIN_RESET);
                 }
-            }
-        }
-
-
-        if (DEFAULT_VOLTAGE_UNITS * orionStatus.maxCellVoltage > MAX_CELL_VOLTAGE && orionStatus.canMsgReceived)
-        {
-            voltagesInRange = 0;
-
-            if (allowCharge) // To avoid wasting time writing to the pin again
-            {
-                allowCharge = 0;
-                chargeContactorOverride = 1;
-                // Turn off charge contactor
-                HAL_GPIO_WritePin(CHARGE_ENABLE_GPIO_Port, CHARGE_ENABLE_Pin, GPIO_PIN_RESET);
-            }
-        }
-
-        if (DEFAULT_VOLTAGE_UNITS * orionStatus.minCellVoltage < MIN_CELL_VOLTAGE && orionStatus.canMsgReceived)
-        {
-            voltagesInRange = 0;
-
-            if (allowDischarge) // To avoid wasting time writing to the pin again
-            {
-                allowDischarge = 0;
-                dischargeContactorOverride = 1;
-                // Turn off High voltage enable and discharge contactor
-                HAL_GPIO_WritePin(HV_ENABLE_GPIO_Port, HV_ENABLE_Pin, GPIO_PIN_RESET);
-                HAL_GPIO_WritePin(DISCHARGE_ENABLE_GPIO_Port, DISCHARGE_ENABLE_Pin, GPIO_PIN_RESET);
             }
         }
 
