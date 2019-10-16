6e8e06180b1797b30a5f2c8aed939c4c1eb13dfb
Baba Adeniran <baba.adeniran@gmail.com>
2019-10-15 23:44:23 -0600

    Putting new CAN report tasks

--- EpsilonAuxBMS/Core/Src/ReportHeartbeatToCanTask.c
+++ EpsilonAuxBMS/Core/Src/ReportHeartbeatToCanTask.c
@@ -7,31 +7,35 @@ static const uint32_t AUX_HEARTBEAT_STDID = 0x720U;
 void reportHeartbeatToCanTask(void const* arg)
 {
     // For concurrency with reportAuxToCanTask()
-    osMutexId* canHandleMutex = (osMutexId*) arg;
+    osMutexId_t* canHandleMutex = (osMutexId_t*) arg;
     // One time osDelayUntil initialization
     uint32_t prevWakeTime = osKernelSysTick();
 
     for (;;)
     {
-        osDelayUntil(&prevWakeTime, AUX_HEARBEAT_FREQ);
+        osDelayUntil(prevWakeTime + AUX_HEARBEAT_FREQ);
 
-        if (osMutexWait(canHandleMutex, 0) != osOK)
+        if (osMutexAcquire(canHandleMutex, 0) != osOK)
         {
             continue;
         }
 
-        hcan1.pTxMsg->DLC = 1;
-        // Set CAN message address
-        hcan1.pTxMsg->StdId = AUX_HEARTBEAT_STDID;
-        hcan1.pTxMsg->DLC = 1;
-        // Always 1
-        hcan1.pTxMsg->Data[0] = 1;
-
-        // Send CAN message
-        if (HAL_CAN_Transmit_IT(&hcan1) == HAL_OK)
-            // Toggle Red LED for every heartbeat sent
+        if (HAL_CAN_GetTxMailboxesFreeLevel(&hcan1) > 0) // Any available Tx mailboxes?
         {
-            HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
+            uint8_t data[1];
+            uint32_t mailbox;
+            // Set CAN message address
+            canTxHeader.StdId = AUX_HEARTBEAT_STDID;
+            canTxHeader.DLC = 1;
+            // Always 1
+            data[0] = 1;
+
+            // Send CAN message
+            if (HAL_CAN_AddTxMessage(&hcan1, &canTxHeader, data, &mailbox) == HAL_OK)
+                // Toggle Red LED for every heartbeat sent
+            {
+                HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
+            }
         }
 
         osMutexRelease(canHandleMutex);
