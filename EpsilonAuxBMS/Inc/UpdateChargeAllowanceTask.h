8e94e99b6bf7bb31da470f59ee2dbee8795077a4
Bill Luu <billluu619@gmail.com>
2020-06-18 23:34:12 -0600

    SFT-154 Change updateChargeAllowanceTask to be more unit test friendly
    Separated the UpdateChargeAllowance function from infinite loop.
    
    Pulled Up CHARGE_ALLOWANCE_UPDATE_FREQ to header for reuse in unit test.

--- EpsilonAuxBMS/Inc/UpdateChargeAllowanceTask.h
+++ EpsilonAuxBMS/Inc/UpdateChargeAllowanceTask.h
@@ -2,8 +2,12 @@
 #include "AuxBmsTasks.h"
 #include "OrionStatus.h"
 
+static const uint32_t CHARGE_ALLOWANCE_UPDATE_FREQ = 100; // Every 100ms
+
 // Task for determining the allowance of charge/discharge based on orion cell voltages
 // It will turn off charge contactor if max cell voltage is too high,
 // and discharge contactor is min cell voltage is too low
 // arg: osMutexId* auxStatusMutex
 void updateChargeAllowanceTask(void const* arg);
+
+void updateChargeAllowance(uint32_t* prevWakeTime);
