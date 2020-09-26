73a03b62782619707b72386e1dac734b02d6ef7e
Baba <baba.adeniran@gmail.com>
2020-09-26 15:21:23 -0600

    SFT 200. Creating tasks relating to CAN Tx work.

--- EpsilonAuxBmsV2/Core/Src/Tasks/SendHeartbeatTask.c
+++ EpsilonAuxBmsV2/Core/Src/Tasks/SendHeartbeatTask.c
@@ -2,5 +2,26 @@
 
 void sendHeartbeatTask(void* arg)
 {
-    return;
+    CanTxGatekeeperQueueData canQueueData = (CanTxGatekeeperQueueData)
+    {
+        0
+    };
+    uint32_t prevWakeTime = osKernelSysTick();
+
+    for (;;)
+    {
+        sendHeartbeat(&canQueueData, &prevWakeTime);
+    }
+}
+
+// Creates a CAN message for reporting the Aux BMS Heartbeat
+void sendHeartbeat(CanTxGatekeeperQueueData* canQueueData, uint32_t* prevWakeTime)
+{
+    canQueueData->canTxHeader = baseCanTxHdr;
+    canQueueData->canTxHeader.StdId = HEARTBEAT_STDID;
+    canQueueData->canTxHeader.DLC = 1;
+    canQueueData->data[0] = 1;
+    osMessageQueuePut(canTxGatekeeperQueue, canQueueData, 0, 0);
+    *prevWakeTime += SEND_HEARTBEAT_TASK_FREQ;
+    osDelayUntil(*prevWakeTime);
 }
