170d4c289d5544c3d17c91e266af5e7cbd8a21ae
jessieguo <jessieguo863@gmail.com>
2022-03-23 15:16:57 -0600

    in v2 we only get one adc value which we use for calculation but in the old code we take in multiple adc reads and check if they're stable before we actually use that for calculation. Wrote code for that

--- EpsilonAuxBmsV2/Core/Inc/Tasks/ContactorFunctions/CheckCurrent.h
+++ EpsilonAuxBmsV2/Core/Inc/Tasks/ContactorFunctions/CheckCurrent.h
@@ -8,4 +8,13 @@ static const int GAIN  = 250;
 static const float CURRENT_LOWER_THRESHOLD  = 0.25; // Lower current threshold (A)
 static const uint32_t ADC_POLL_TIMEOUT  = 10;
 
+static const int MAX_SENSE_DIFF = 10; // The maximum difference between senses read from current amplifier
+static const int MAX_CURRENT_READ_ATTEMPTS = 50; // The maximum number of times to try obtaining a stable CURRENT_SENSE
+static const uint32_t SENSE_SETUP_TIME  = 50; // Setup time for current_sense
+static const int NUM_SENSES = 3; // The number of senses to take at a time when reading current amplifier
+
+
 uint8_t isCurrentLow(uint8_t numContactorsClosed);
+
+int isCurrentStable(uint32_t senses[]);
+uint32_t readCurrentThroughContactors(void);
