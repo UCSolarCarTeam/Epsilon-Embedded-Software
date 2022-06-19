27f913d6a627dd5a3f3c39bf4031ba7f7c1cc10e
Jessie Guo <jessieguo863@gmail.com>
2022-06-19 16:30:06 -0600

    setting strobeBms in disconnect contactor function which keeps it on for some reason except it makes no logical sense, keeping it there for now until further investigation

--- EpsilonAuxBmsV2/Core/Inc/Tasks/ContactorFunctions/DisconnectContactors.h
+++ EpsilonAuxBmsV2/Core/Inc/Tasks/ContactorFunctions/DisconnectContactors.h
@@ -2,11 +2,15 @@
 #include "cmsis_os.h"
 #include "ContactorEventFlags.h"
 #include "ContactorState.h"
+#include "AuxStatus.h"
+#include "main.h"
 
 extern osEventFlagsId_t contactorControlEventBits;
 extern AuxBmsContactorState auxBmsContactorState;
 extern osThreadId_t commonContactorGatekeeperTaskHandle;
 extern osThreadId_t chargeContactorGatekeeperTaskHandle;
 extern osThreadId_t dischargeContactorGatekeeperTaskHandle;
+extern AuxStatus auxStatus;
+extern osMutexId_t auxStatusOrionInterfaceMutex;
 
 void disconnectContactors();
